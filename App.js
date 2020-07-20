3fdbc3bdf40e17981b44ffc496ddd387f4322426
EmanAALK <emanalkandarii@gmail.com>
2020-07-20 20:52:52 +0300

    Create function POST

--- App.js
+++ App.js
@@ -1,16 +1,47 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 //Data
-const plants = require("./data");
+let plants = require("./data");
 
 //Create Express App Instance
 const app = express();
+app.use(bodyParser.json());
 
 app.use(cors());
 
 app.get("/plants", (req, res) => {
   res.json(plants);
+});
+
+//Create Plant
+app.post("/plants", (req, res) => {
+  const id = plants[plants.length - 1].id + 1;
+  const slug = slugify(req.body.name, { lower: true });
+  const newPlant = { id, slug, ...req.body };
+  plants.push(newPlant);
+  res.status(201).json(newPlant);
+});
+
+app.post("/cookies", (req, res) => {
+  const id = cookies[cookies.length - 1].id + 1;
+  const newCookie = { id, ...req.body }; //id is equivalent to id: id
+  cookies.push(newCookie);
+  res.json(newCookie);
+});
+
+//Delete Plant
+app.delete("/plants/:plantId", (req, res) => {
+  const { plantId } = req.params;
+  const foundPlant = plants.find((plant) => plant.id === +plantId);
+  if (foundPlant) {
+    plants = plants.filter((plant) => plant.id !== +plantId);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Plant not Found" });
+  }
 });
 
 app.listen(8000);
