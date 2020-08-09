e354eb50009b0e2d9f5a9e6588014514e6403a7b
EmanAALK <emanalkandarii@gmail.com>
2020-08-09 18:38:02 +0300

    Done :D

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup } = require("../controllers/userController");
+const { signup } = require("../controllers/userControllers");
 
 router.post("/signup", signup);
 
