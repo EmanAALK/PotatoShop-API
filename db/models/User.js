f0de0c39bc0a8a5d801fde5bc17bc4d48272fb39
EmanAALK <emanalkandarii@gmail.com>
2020-08-09 17:34:29 +0300

    user model

---
+++ db/models/User.js
@@ -0,0 +1,35 @@
+const { DataTypes, Model } = require("sequelize");
+const db = require("../db");
+
+class User extends Model {}
+
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    sequelize: db,
+  }
+);
+
+module.exports = User;
