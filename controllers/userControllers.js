4969dbd4bb61785f896b8e39278837809eea165c
EmanAALK <emanalkandarii@gmail.com>
2020-08-09 17:50:05 +0300

    passowrd encrypting method

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,15 @@
+const bcrypt = require("bcrypt");
+
+//Database
 const { User } = require("../db/models");
 
 exports.signup = async (req, res, next) => {
+  const { password } = req.body;
+  const saltRounds = 10;
   try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     const newUser = await User.create(req.body);
-    res.json(newUser);
+    res.json({ message: "User created successfully" });
   } catch (error) {
     next(error);
   }
