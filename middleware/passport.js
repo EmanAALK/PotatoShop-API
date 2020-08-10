31a50fa711f4248292d5a221e8429815dfc29af9
EmanAALK <emanalkandarii@gmail.com>
2020-08-10 11:40:23 +0300

    Stamped

---
+++ middleware/passport.js
@@ -0,0 +1,17 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+//Modal
+const { User } = require("../db/models/");
+
+exports.localStrategy = new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await User.findOne({ where: { username: username } });
+    const password = user
+      ? await bcrypt.compare(password, user.password)
+      : false;
+    passwordMatch ? done(null, user) : done(null, false);
+  } catch (error) {
+    done(error);
+  }
+});
